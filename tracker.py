692602692075dae41a66ef7ccfaaf260d192bea9
Exortions <75327059+Exortions@users.noreply.github.com>
2022-10-26 20:53:07 -0700

    make sure tracker.py can't be imported
    Hello,
    
    I've made a few minor changes that you don't have to implement but improve the code quality.
     - Make sure tracker.py can't be imported by only running the program if the `python3` command directly executes it.
     - Change double quotes to single ones to match the code style.

--- tracker.py
+++ tracker.py
@@ -11,25 +11,26 @@ def track():
         screenshot_name = take_screenshot()
         process_screenshot_file(screenshot_name)
     except:
-        print("track() broke")
+        print('track() broke')
 
 
-tab_pressed_time = 0
-while True:
-    try:
-        if keyboard.is_pressed('tab'):
-            print('Tab pressed!')
-            tab_pressed_time += 1
-            time.sleep(0.1)
-            if (tab_pressed_time == 2):  # only track once at 2 ticks, then wait until tab is released again
-                tab_pressed_time = 0
-                print('track')
-                track()
-                time.sleep(10)  # wait a bit before tracking again
-            continue
+if __name__ == '__main__':
+    tab_pressed_time = 0
+    while True:
+        try:
+            if keyboard.is_pressed('tab'):
+                print('Tab pressed!')
+                tab_pressed_time += 1
+                time.sleep(0.1)
+                if (tab_pressed_time == 2):  # only track once at 2 ticks, then wait until tab is released again
+                    tab_pressed_time = 0
+                    print('track')
+                    track()
+                    time.sleep(10)  # wait a bit before tracking again
+                continue
 
-        tab_pressed_time = 0
-        time.sleep(1)
-    except:
-        print("loop broke")
-        break
+            tab_pressed_time = 0
+            time.sleep(1)
+        except:
+            print('loop broke')
+            break
