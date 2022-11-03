c2907fece9b91c3d88e8c06b7a42d1574aca8cb9
inventivetalent <mail@inventivetalent.org>
2022-11-03 11:45:20 +0100

    resize to 1920x1080 (#3)

--- screenshot.py
+++ screenshot.py
@@ -1,5 +1,6 @@
 import configparser
 
+from PIL import Image
 from mss import mss
 
 config = configparser.ConfigParser()
@@ -12,4 +13,10 @@ def take_screenshot():
     with mss() as sct:
         sct.shot(mon=monitor)
     print("*snap*")
+
+    # resize to 1920x1080
+    image = Image.open(f"monitor-{monitor}.png")
+    image.thumbnail((1920, 1080), Image.ANTIALIAS)
+    image.save(f"monitor-{monitor}.png", "png")
+
     return f"monitor-{monitor}.png"
