a14c5118e50cad5a97a8f5d1467b25f2e7fa77dc
inventivetalent <mail@inventivetalent.org>
2022-11-06 23:10:05 +0100

    separate csv outputs

--- output.py
+++ output.py
@@ -31,29 +31,69 @@ def write_to_json(result):
 
 
 def append_to_csv(result):
-    fields = [result['time'], result['match']['mode'], result['match']['map'], result['match']['time'], result['self']['hero']]
+    all_fields = [result['time'], result['match']['mode'], result['match']['map'], result['match']['competitive'], result['match']['time'], result['self']['hero'], result['state']]
+
+    ally_total_elims = 0
+    ally_total_assists = 0
+    ally_total_deaths = 0
+    ally_total_dmg = 0
+    ally_total_heal = 0
+    ally_total_mit = 0
 
     for i in range(0, 5):
-        fields.append(result['players']['allies'][i]['name'])
-        fields.append(result['players']['allies'][i]['elims'])
-        fields.append(result['players']['allies'][i]['assists'])
-        fields.append(result['players']['allies'][i]['deaths'])
-        fields.append(result['players']['allies'][i]['dmg'])
-        fields.append(result['players']['allies'][i]['heal'])
-        fields.append(result['players']['allies'][i]['mit'])
+        ally_total_elims += result['players']['allies'][i]['elims']
+        ally_total_assists += result['players']['allies'][i]['assists']
+        ally_total_deaths += result['players']['allies'][i]['deaths']
+        ally_total_dmg += result['players']['allies'][i]['dmg']
+        ally_total_heal += result['players']['allies'][i]['heal']
+        ally_total_mit += result['players']['allies'][i]['mit']
+
+        all_fields.append(result['players']['allies'][i]['name'])
+        all_fields.append(result['players']['allies'][i]['elims'])
+        all_fields.append(result['players']['allies'][i]['assists'])
+        all_fields.append(result['players']['allies'][i]['deaths'])
+        all_fields.append(result['players']['allies'][i]['dmg'])
+        all_fields.append(result['players']['allies'][i]['heal'])
+        all_fields.append(result['players']['allies'][i]['mit'])
+
+    enemy_total_elims = 0
+    enemy_total_assists = 0
+    enemy_total_deaths = 0
+    enemy_total_dmg = 0
+    enemy_total_heal = 0
+    enemy_total_mit = 0
 
     for i in range(0, 5):
-        fields.append(result['players']['enemies'][i]['name'])
-        fields.append(result['players']['enemies'][i]['elims'])
-        fields.append(result['players']['enemies'][i]['assists'])
-        fields.append(result['players']['enemies'][i]['deaths'])
-        fields.append(result['players']['enemies'][i]['dmg'])
-        fields.append(result['players']['enemies'][i]['heal'])
-        fields.append(result['players']['enemies'][i]['mit'])
+        enemy_total_elims += result['players']['enemies'][i]['elims']
+        enemy_total_assists += result['players']['enemies'][i]['assists']
+        enemy_total_deaths += result['players']['enemies'][i]['deaths']
+        enemy_total_dmg += result['players']['enemies'][i]['dmg']
+        enemy_total_heal += result['players']['enemies'][i]['heal']
+        enemy_total_mit += result['players']['enemies'][i]['mit']
 
-    with open(r'log.csv', 'a', newline='') as f:
+        all_fields.append(result['players']['enemies'][i]['name'])
+        all_fields.append(result['players']['enemies'][i]['elims'])
+        all_fields.append(result['players']['enemies'][i]['assists'])
+        all_fields.append(result['players']['enemies'][i]['deaths'])
+        all_fields.append(result['players']['enemies'][i]['dmg'])
+        all_fields.append(result['players']['enemies'][i]['heal'])
+        all_fields.append(result['players']['enemies'][i]['mit'])
+
+    with open(r'log-all.csv', 'a', newline='') as f:
         writer = csv.writer(f)
-        writer.writerow(fields)
+        writer.writerow(all_fields)
+
+    total_fields = [result['time'], result['match']['mode'], result['match']['map'], result['match']['competitive'], result['match']['time'], result['self']['hero'], result['state'],
+                    ally_total_elims, enemy_total_elims, ally_total_assists, enemy_total_assists, ally_total_dmg, enemy_total_dmg, ally_total_heal, enemy_total_heal, ally_total_mit, enemy_total_mit]
+
+    with open(r'log-total.csv', 'a', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(total_fields)
+
+    if (result['state'] == 'win' or result['state'] == 'loss' or result['state'] == 'draw'):
+        with open(r'log-result.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(total_fields)
 
 
 influx_client = InfluxDBClient.from_config_file("config.ini")
